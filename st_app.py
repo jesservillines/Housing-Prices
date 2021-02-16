55729f194a905b03ef968003c4346d7a6e7e2e53
jesservillines <jesservillines@gmail.com>
2021-02-16 15:25:52 -0700

    streamlit

--- st_app.py
+++ st_app.py
@@ -3,20 +3,19 @@ import numpy as np
 import pandas as pd
 import pickle
 import streamlit as st
-from sklearn.linear_model import LinearRegression
 
 st.set_page_config(
-  page_icon='📖',
-  initial_sidebar_state='expanded'
+    page_icon='📖',
+    initial_sidebar_state='expanded'
 )
 
-st.title('The Home buyers Toolbox!')
+st.title('Ames Predict-O-Matic!')
 
 st.write('Use the sidebar to select a page to view.')
 
 page = st.sidebar.selectbox(
-  'Page',
-  ('Home', 'Form')
+    'Page',
+    ('Home', 'Form')
 )
 
 # @st.cache
@@ -25,29 +24,29 @@ page = st.sidebar.selectbox(
 #     return df
 
 if page == 'Home':
-  st.subheader('Home Page')
-  st.write('Hello, World, welcome to the Home Buys Toolbox!')
+    st.subheader('Home Page')
+    st.write('Hello, World, to my sliiiiick Streamlit App!')
 
 if page == 'Form':
-  # header
-  st.subheader('Home Qualities')
-  st.write('''Input the qualities of the home
-  for which you want the price predicted.''')
+    # header
+    st.subheader('Home Qualities')
+    st.write('''Input the qualities of the home
+    for which you want the price predicted.''')
 
-  # get user input
-  qual = st.number_input('Overall Quality', format='%d', min_value=int(0), value=int(0))
-  bath = st.number_input('Number of Bathrooms', format='%d', min_value=int(0), value=int(0))
-  garage = st.number_input('Garage Area', format='%d', min_value=int(0), step=100, value=int(0))
-  lot = st.number_input('Lot Area', format='%d', min_value=int(0), step=1000, value=int(0))
+    # get user input
+    qual = st.number_input('Neighboorhood Quality Index', format='%d', min_value=int(0), value=int(0))
+    bath = st.number_input('Local Positive Features', format='%d', min_value=int(0), value=int(0))
+    garage = st.number_input('Single Story Building', format='%d', min_value=int(0), step=100, value=int(0))
+    lot = st.number_input('Multiple Story Building', format='%d', min_value=int(0), step=1000, value=int(0))
 
-  data = np.array([qual, bath, garage, lot]).reshape(1, -1)
+    data = np.array([qual, bath, garage, lot]).reshape(1, -1)
 
-  st.subheader('Make a prediction')
+    st.subheader('Make a prediction')
 
-  with open('./model/model.p', 'rb') as pickle_in:
-      model = pickle.load(pickle_in)
+    with open('./model/model.p', 'rb') as pickle_in:
+        model = pickle.load(pickle_in)
 
-  predicted_price = model.predict(data)[0]
+    predicted_price = model.predict(data)[0]
 
-  st.subheader('Results:')
-  st.write(f'Your home is worth {round(predicted_price, 2)}. Go you!')
+    st.subheader('Results:')
+    st.write(f'Your home is worth {round(predicted_price, 2)}. Go you!')
